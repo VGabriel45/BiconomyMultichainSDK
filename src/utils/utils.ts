155860cdd587f5495f60afc63cc596bf33351153
GabiDev <gv@popoo.io>
2023-10-24 18:30:12 +0300

    Removed unused code and files

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,18 +1,5 @@
 import { ChainId } from "@biconomy/core-types"
-// import logger from "./logger";
 
 export const defaultBundleUrlByChainId = (chainId: ChainId) => {
     return `https://bundler.biconomy.io/api/v2/${chainId}/nJPK7B3ru.dd7f7861-190d-41bd-af80-6877f74b8f44`;
 }
-
-// export const logConsole = (message: string, isError: boolean) => {
-//     if(process.env.NODE_ENV === 'test'){
-//         if(isError){
-//             logger.error(message)
-//         } else {
-//             logger.info(message)
-//         }
-//     } else {
-//         console.log(message);
-//     }
-// }
